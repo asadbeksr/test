b91488019b7999ee53a2ce12cec6eda2998e8a54
bek <asadbeksr@gmail.com>
2023-02-18 22:37:30 +0500

    modifies UserContecxt test

--- src/tests/UserContext.test.tsx
+++ src/tests/UserContext.test.tsx
@@ -1,40 +1,40 @@
-import { render, screen } from '@testing-library/react'
-import Sidebar from '@ui/Sidebar'
-import { ThemeProvider } from '@mui/material'
-import { AuthContext } from '@components/Contexts/AuthContext'
-import { BrowserRouter } from 'react-router-dom'
-import {theme} from '.././theme'
+import { render, screen } from '@testing-library/react';
+import Sidebar from '@ui/Sidebar';
+import { ThemeProvider } from '@mui/material/styles';
+import { AuthContext } from '@components/Contexts/AuthContext';
+import { BrowserRouter } from 'react-router-dom';
+import { theme } from '../theme';
 
-describe('User', () => {
-  it('not logged in user', () => {
+describe('Sidebar', () => {
+  test('should not display user card for logged out user', () => {
     const user = {
       user: null,
-      login: () => null,
-      logout: () => null,
-    }
+      login: () => {},
+      logout: () => {},
+    };
 
     render(
       <BrowserRouter>
         <AuthContext.Provider value={user}>
-        <ThemeProvider theme={theme}>
-          <Sidebar />
-        </ThemeProvider>
+          <ThemeProvider theme={theme}>
+            <Sidebar />
+          </ThemeProvider>
         </AuthContext.Provider>
       </BrowserRouter>
-    )
+    );
 
-    expect(screen.queryByTestId('user-card')).not.toBeInTheDocument()
-  })
+    expect(screen.queryByTestId('user-card')).toBeNull();
+  });
 
-  it('render user card', () => {
+  test('should display user card for logged in user', () => {
     const user = {
       user: {
         email: 'email',
         username: 'username',
       },
-      login: () => null,
-      logout: () => null,
-    }
+      login: () => {},
+      logout: () => {},
+    };
 
     render(
       <BrowserRouter>
@@ -42,8 +42,8 @@ describe('User', () => {
           <Sidebar />
         </AuthContext.Provider>
       </BrowserRouter>
-    )
+    );
 
-    expect(screen.getByText('username')).toBeInTheDocument()
-  })
-})
+    expect(screen.getByText('username')).toBeInTheDocument();
+  });
+});
